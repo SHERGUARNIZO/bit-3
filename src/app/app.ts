2f65af5a32e77ce197d9fcaba5d36d5c1ca8cb0e
Sherly Guarnizo <sherlyguarnizo@gmail.com>
2025-06-23 21:29:14 -0500

    project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header }       from './shared/header/header';
+import { Footer }       from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ Header, RouterOutlet ],
+  imports: [ Header, RouterOutlet, Footer ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
